f59905bfbeb20789f3d47591d50ce2b4fd1ee560
dedmazayukr <dedmazyukr@gmail.com>
2018-02-05 23:24:33 +0200

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    "semi": [1, "always"],
+    "no-extra-semi": "warn",
+    "no-use-before-define": ["warn"],
+    "no-unused-vars": "warn"
+  }
+};
